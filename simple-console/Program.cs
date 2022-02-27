9aa0ce0888cc1e662d9b5238b5e2bddc2309892f
Bruno Sonnino <sonnino@revolution.com.br>
2022-02-27 13:41:44 -0500

    Initial commit

---
+++ simple-console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+var handler = new HttpClientHandler();
+handler.ServerCertificateCustomValidationCallback =
+              (message, cert, chain, errors) => 
+              { return true; };
+using var client = new HttpClient(handler);
+var json = await client.GetStringAsync("https://localhost:7219/weatherforecast");
+var forecasts = JsonSerializer.Deserialize<List<WeatherForecast>>(json);
+if (forecasts == null)
+    return;
+foreach (var forecast in forecasts)
+    Console.WriteLine($"{forecast.date:dd/MM/yyyy}  {forecast.temperatureC}  {forecast.summary}");
+
+public class WeatherForecast
+{
+    public DateTime date { get; set; }
+    public int temperatureC { get; set; }
+    public int temperatureF => 32 + (int)(temperatureC / 0.5556);
+    public string? summary { get; set; }
+} 
